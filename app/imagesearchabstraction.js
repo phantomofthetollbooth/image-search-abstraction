9f9461025561cdae3ea00786787cc4c5c61301ff
X X <X X>
2016-05-13 22:03:20 -0500

    testing output

--- app/imagesearchabstraction.js
+++ app/imagesearchabstraction.js
@@ -27,7 +27,7 @@ request(requestOptions, function (error, response, body) {
   if (error) {
 	  console.log(error)
   }else{
-	 res.json(JSON.parse(body).data;
+	 res.send(JSON.parse(body).data;
 	
   }
 })
